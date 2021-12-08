5f5a82c3f9b37114d7d76f322c58fe54208cd2bd
Steve Bronder <stevo15025@gmail.com>
2021-12-08 18:29:25 -0500

    add note about github_pat in R script

--- README.md
+++ README.md
@@ -8,12 +8,27 @@ Use the following commands to download and install this repo
 git clone --recursive git@github.com:SteveBronder/pathfinder_testing.git
 # if you don't have ssh enabled on your local git use the following 
 # git clone --recursive https://github.com/SteveBronder/pathfinder_testing.git 
-cd cmdstan 
+cd pathfinder_testing/cmdstan
 echo "STAN_THREADS=true" > make/local 
 echo "O=3 -march=native -mtune=native" >> make/local 
 make -j4 build
 cd .. 
-R -s -e "remotes::install_local(path = './cmdstanr')"
+R -s -e "remotes::install_local(path = './cmdstanr', force = TRUE)"
+```
+
+If you have the necessary packages
+
+```R
+library(posteriordb)
+library(ggplot2)
+library(ggExtra)
+library(data.table)
+```
+
+The following script should run without error
+
+```bash
+Rscript ./examples/posterior_db/posterior_db_ex.R 
 ```
 
 Hopefully, that should be it and you can then run the examples in this folder!
--- examples/posterior_db/posterior_db_ex.R
+++ examples/posterior_db/posterior_db_ex.R
@@ -8,6 +8,14 @@ pp = function(extra = NULL) {paste0("./cmdstan/", extra)}
 
 cmdstanr::set_cmdstan_path(pp())
 
+# NOTE: This is going to fail eventually if we ping it too many times
+# To increase the github rate limit follow the instruction at [1] to get
+#  a github PAT (Personal Authentication Token) token.
+#  Then add that token to your R environment variables with something like
+# echo "Sys.setenv(GITHUB_PAT='PASTE_YOUR_TOKEN_HERE')" >> ~/.Rprofile
+# You do not need to give the token any auths
+# and make sure you do not accidentally share it that would be very very bad!!!
+# [1] https://www.edgoad.com/2021/02/using-personal-access-tokens-with-git-and-github.html
 pdb_conn <- pdb_github()
 
 pdb_names = posterior_names(pdb_conn)
@@ -18,12 +26,17 @@ ex_posterior = posteriordb::posterior("hmm_example-hmm_example", pdb_conn)
 ex_code_path = model_code_file_path(ex_posterior, framework = "stan")
 ex_mod = cmdstanr::cmdstan_model(ex_code_path)
 ex_data_file_path = data_file_path(ex_posterior)
+
+# Run path and sampler
 ex_fit = ex_mod$pathfinder(algorithm = "multi", data = ex_data_file_path,
-  refresh = 0, num_threads = floor(parallel::detectCores() / 2), num_paths = 4,
+  refresh = 5, num_threads = floor(parallel::detectCores() / 2), num_paths = 5,
   psis_draws = 4000, iter = 100)
 
 ex_fit_sample = ex_mod$sample(data = ex_data_file_path,
-  parallel_chains = 4, output_dir = "./examples/posterior_db/")
+  parallel_chains = 4)
+
+
+# Have a lookie
 
 ex_fit$summary()
 ex_fit_sample$summary()
@@ -50,9 +63,15 @@ for (base_param_name in orig_names) {
     xlim(axis_limits) + ylim(axis_limits) +
     ggtitle(paste0("Comparison of ", base_param_name))
   marg_plot = ggMarginal(base_plot, type = "histogram", xparams = list(bins=50), fill = "red")
-  print(marg_plot)
-  user_inp = readline(prompt="Press [enter] to continue or enter q to quit")
-  if (user_inp == "q") {
+  if (interactive()) {
+    print(marg_plot)
+    user_inp = readline(prompt="Press [enter] to continue or enter q to quit")
+    if (user_inp == "q") {
+      break
+    }
+  } else {
+    print("Idk how to show graphs with Rscript :-(")
     break
   }
 }
+
